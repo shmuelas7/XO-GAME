9ea8c303981c3772696fadae7bf7f7d061167562
avraham schmidt <avibatst@gmail.com>
2022-05-17 20:00:50 +0300

    end 17

--- gaem.js
+++ gaem.js
@@ -1,15 +1,31 @@
+let reset = document.getElementById("reset");
+let newGame = document.getElementById("newGame");
+newGame.onclick = () => {
+  localStorage.setItem("FirstPlayer", countX);
+  localStorage.setItem("SecndPlayer", county);
+  document.location.reload(true);
+};
+reset.onclick = () => {
+  localStorage.clear();
+  document.location.reload(true);
+};
+// let s =
+let countX = localStorage.getItem("FirstPlayer");
+let county = localStorage.getItem("SecndPlayer");
+
 let list = [];
 let num = 3;
 for (let i = 0; i < num; i++) {
   list[i] = [];
   for (let j = 0; j < num; j++) {
-    // debugger;
-    list[i][j] = null;
+    list[i][j] = " ";
   }
 }
 
 let count = 0;
 let x, y;
+let win = false;
+let winer = [,];
 const board = document.getElementById("board");
 for (i = 0; i < num; i++) {
   let div = document.createElement("div");
@@ -22,19 +38,24 @@ for (i = 0; i < num; i++) {
     col.innerHTML = "a";
     col.id = JSON.stringify({ i, j });
     col.onclick = (e) => {
-      const { i, j } = JSON.parse(e.target.id);
-      if (count % 2 == 0) {
-        e.target.innerHTML = "X";
-        list[i][j] = "X";
-
-        checkList("X", list);
-      } else {
-        e.target.innerHTML = "O";
-        list[i][j] = "O";
-        checkList("O", list);
+      if (e.target.innerHTML != "X" && e.target.innerHTML != "O" && !win) {
+        console.log(countX);
+        console.log(county);
+        const { i, j } = JSON.parse(e.target.id);
+        if (count % 2 == 0) {
+          countX++;
+          e.target.innerHTML = "X";
+          list[i][j] = "X";
+          checkList("X", list);
+        } else {
+          county++;
+          e.target.innerHTML = "O";
+          list[i][j] = "O";
+          checkList("O", list);
+        }
+        // console.log(list);
+        count++;
       }
-      console.log(list);
-      count++;
     };
     div.appendChild(col);
   }
@@ -42,7 +63,12 @@ for (i = 0; i < num; i++) {
 }
 
 function chec(list, ch) {
-  return list.every((v) => v === ch);
+  if (
+    list.every((v) => {
+      v === ch && v != " ";
+    })
+  )
+    win = true;
 }
 
 let lis = ["", "", ""];
@@ -51,10 +77,13 @@ let listSlantL = ["", "", ""];
 function checkList(check, list) {
   //   debugger;
   for (let i = 0; i < num; i++) {
-    if (chec(list[i], check)) {
-      alert("win");
-      return;
-    }
+    // debugger;
+    chec(list[i], check);
+    // if (chec(list[i], check)) {
+    //   // alert("win");
+    //   win = 1;
+    //   return;
+    // }
     for (let j = 0; j < num; j++) {
       lis[j] = list[j][i];
       if (i == j) {
@@ -63,13 +92,20 @@ function checkList(check, list) {
       if (i == num - 1 - j) {
         listSlantL[i] = list[i][j];
       }
-      if (chec(lis, check)) {
-        console.log("win");
-      } else if (chec(listSlantR, check)) {
-        console.log("win");
-      } else if (chec(listSlantL, check)) {
-        console.log("win");
-      }
+      chec(lis, check);
+      chec(listSlantR, check);
+      chec(listSlantL, check);
+      // if (chec(lis, check)) {
+      //   // console.log("win");
+      //   win = true;
+      // } else if (chec(listSlantR, check)) {
+      //   // console.log("win");
+      //   win = 1;
+      // } else if (chec(listSlantL, check)) {
+      //   // console.log("win");
+      //   win = 1;
+      // }
     }
   }
 }
+// }
